0d19d276966894e9b6d4cd3ef146db89d737dc48
Symoon Zadid <symoonzadid@gmail.com>
2021-09-20 22:48:30 +0600

    Initial Commit

---
+++ MyBanglalinkUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBanglalinkUI
+//
+//  Created by BS-125 on 20/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
